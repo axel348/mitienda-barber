10bf3c938c1dda16f81677eef4a073f690ab2adf
Axel Duran <ax.duranr@duocuc.cl>
2025-11-24 15:37:54 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    open: true, // Abre automáticamente el navegador
-    hmr: true, // Hot Module Replacement habilitado
-    watch: {
-      usePolling: true, // Útil en Windows para detectar cambios
-    }
-  },
-  build: {
-    sourcemap: true
-  }
 })
